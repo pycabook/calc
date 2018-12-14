b3073d85fde0021a1e252fe1387a9ea0644e7f78
Leonardo Giordani <giordani.leonardo@gmail.com>
2018-12-14 14:55:46 +0000

    Step 5 - Multiply two numbers the smart way

--- calc/calc.py
+++ calc/calc.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 class Calc:
     def add(self, *args):
         return sum(args)
@@ -5,5 +8,8 @@ class Calc:
     def sub(self, a, b):
         return a - b
 
-    def mul(self, a, b):
-        return a * b
+    def mul(self, *args):
+        def mul2(a, b):
+            return a * b
+
+        return reduce(mul2, args)
