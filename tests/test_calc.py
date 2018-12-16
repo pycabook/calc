c53bc6ae7390e57e8b8133da4b18e3fd7d1a12dc
Leonardo Giordani <giordani.leonardo@gmail.com>
2018-12-16 10:16:05 +0000

    Step 9.7 - Empty list after thresholds

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -116,3 +116,11 @@ def test_avg_empty_list():
     res = c.avg([])
 
     assert res == 0
+
+
+def test_avg_manages_empty_list_after_outlier_removal():
+    c = Calc()
+
+    res = c.avg([12, 98], lt=15, ut=90)
+
+    assert res == 0
